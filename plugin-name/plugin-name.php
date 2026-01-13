7354c7ad9140a872ab4599eacf33388ff37312c2
peanutsx50 <murimifestus09@gmail.com>
2026-01-13 17:14:10 +0300

    Add Composer support and update .gitignore for generated files
    - Introduced composer.json for dependency management and autoloading.
    - Added setup.php for post-create project setup.
    - Updated .gitignore to exclude vendor directory and composer.lock.

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -26,23 +26,34 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (! defined('WPINC')) {
 	die;
+}
+
+// Autoload dependencies using Composer
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+	require_once __DIR__ . '/vendor/autoload.php';
 }
 
 /**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
+ * 
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+
+// setting up plugin constants
+define('PLUGIN_NAME_VERSION', '1.0.0');
+define('PLUGIN_NAME_URL', plugin_dir_url(__FILE__));
+define('PLUGIN_NAME_PATH', plugin_dir_path(__FILE__));
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-plugin-name-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+function activate_plugin_name()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name-activator.php';
 	Plugin_Name_Activator::activate();
 }
 
@@ -50,19 +61,20 @@ function activate_plugin_name() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-plugin-name-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+function deactivate_plugin_name()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-plugin-name-deactivator.php';
 	Plugin_Name_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook(__FILE__, 'activate_plugin_name');
+register_deactivation_hook(__FILE__, 'deactivate_plugin_name');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path(__FILE__) . 'includes/class-plugin-name.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,10 +85,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_plugin_name()
+{
 
 	$plugin = new Plugin_Name();
 	$plugin->run();
-
 }
 run_plugin_name();
---
+++ plugin-name/setup.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Setup file for Plugin Name
+ * 
+ * This script runs after composer create-project
+ */
