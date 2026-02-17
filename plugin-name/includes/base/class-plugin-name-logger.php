68c28af8004216e0fe54c08c89d3e5cf2159ca43
peanutsx50 <murimifestus09@gmail.com>
2026-02-17 16:53:47 +0300

    feat: Add logger class for the plugin to handle message logging

---
+++ plugin-name/includes/base/class-plugin-name-logger.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Provide a logger class for the plugin
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ */
+
+/**
+ * Provide a logger class for the plugin.
+ * 
+ * This class defines all code necessary to log messages for the plugin.
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/includes
+ * @author     Your Name <email@example.com>
+ */
+
+namespace PluginName\base;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+
+class Plugin_Name_Logger
+{
+
+}
